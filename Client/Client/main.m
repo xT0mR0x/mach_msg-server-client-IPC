87a0014e48c5d1d907684daa2309303f9258b178
xT0mR0x <112822060+xT0mR0x@users.noreply.github.com>
2022-12-07 20:26:20 +0200

    Update main.m

--- Client/Client/main.m
+++ Client/Client/main.m
@@ -31,3 +31,19 @@ typedef struct {
   Message message;
  mach_msg_trailer_t trailer;
 } ReceiveMessage;
+
+
+// Receive message routine:
+ mach_msg_return_t
+ receive_msg(mach_port_name_t recvPort, mach_msg_timeout_t timeout,ReceiveMessage *receiveMessage) {
+  mach_msg_return_t ret = mach_msg(
+      /* msg */ (mach_msg_header_t *)receiveMessage,
+      /* option */ MACH_RCV_MSG | MACH_RCV_TIMEOUT,
+      /* send size */ 0,
+      /* recv size */ sizeof(*receiveMessage),
+      /* recv_name */ recvPort,
+      /* timeout */ timeout,
+      /* notify port */ MACH_PORT_NULL);
+  if (ret != MACH_MSG_SUCCESS) {
+    return ret;
+  }
