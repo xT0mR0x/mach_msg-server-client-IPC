7a16531d29e44a7c59aa1c2bbf672462112a8302
xT0mR0x <112822060+xT0mR0x@users.noreply.github.com>
2022-12-07 15:08:23 +0200

    Update main.m

--- Client/Client/main.m
+++ Client/Client/main.m
@@ -2,26 +2,24 @@
 //  main.m
 //  Client
 //
-//  Created by Tom Rosenzweig on 07/12/2022.
-//
+//  Created by Tom Rosenzweig
 
-
-#include <bootstrap.h>
-#include <mach/message.h>
-#include <mach/mach_init.h>
-#include <mach/mach_port.h>
-#include <mach/port.h>
-#include <mach/task.h>
-#include <Foundation/NSProcessInfo.h>
-#include <Foundation/NSString.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#define MS_IN_S 1000
+  #include <bootstrap.h>
+  #include <mach/message.h>
+  #include <mach/mach_init.h>
+  #include <mach/mach_port.h>
+  #include <mach/port.h>
+  #include <mach/task.h>
+  #include <Foundation/NSProcessInfo.h>
+  #include <Foundation/NSString.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <unistd.h>
+  #define MS_IN_S 1000
 
 // Message structure:
 typedef struct {
-  mach_msg_header_t header;
+mach_msg_header_t header;
   char Message_Body[1024];
   int Message_Size;
 } Message;
